4b25656a86feb079d0f949986706efe90dd2ee0c
debbie <drosenfeld87@gmail.com>
2018-01-30 20:58:13 -0500

    completed exercise6

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,7 @@
 def temperature (number)
+  puts "temperature in celcius is #{(number.to_i - 32) * 5 / 9}"
+end
 
 puts "enter temperature in fahrenheit"
-  temperature = gets.chomp
+number = gets.chomp
+temperature(number)
